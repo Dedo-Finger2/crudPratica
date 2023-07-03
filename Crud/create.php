fa8b548b6edfa354a3cb482d0e79ab1ac276131e
Dedo_Finger2 <antonioimportant@gmail.com>
2023-07-03 10:16:33 -0300

    create done

--- Crud/create.php
+++ Crud/create.php
@@ -12,19 +12,19 @@ $dadosProfissao = [
 /**
  * Inserindo uma nova profissão no banco de dados
  */
-$createProfissão = $conn->prepare("INSERT INTO profissao(nome) VALUES (?)");
-$createProfissão->bind_param("s", $dadosProfissao['nome']);
-$createProfissão->execute();
+$createProfissao = $conn->prepare("INSERT INTO profissao(nome) VALUES (?)");
+$createProfissao->bind_param("s", $dadosProfissao['nome']);
+// $createProfissao->execute();
 
 echo "<h1>Nova profissão</h1>";
-var_dump($createProfissão);
+// var_dump($createProfissao);
 
 /**
  * Obtendo o ID dessa nova profissão
  */
-$idProfssaoConsulta = $conn->query("SELECT idProfissao FROM profissao WHERE nome = 'Baleiro'");
-$idProfssaoArray = $idProfssaoConsulta->fetch_assoc();
-$idProfissao = $idProfssaoArray['idProfissao'];
+$profssaoConsulta = $conn->query("SELECT * FROM profissao");
+$idProfissaoArray = $profssaoConsulta->fetch_assoc();
+$idProfissao = $idProfissaoArray['idProfissao'];
 
 /**
  * Dados para se criar um novo usuário
@@ -32,16 +32,32 @@ $idProfissao = $idProfssaoArray['idProfissao'];
 $dadosUsuario = [
     "nome" => "Wendson",
     "dataNascimento" => "2023-07-03",
-    "profissao" => $createProfissão->insert_id
+    "profissao" => $createProfissao->insert_id
 
 ];
 
 /**
  * Inserindo um novo usuário no banco de dados
+ * Usuario possui uma relação com profissão, uma de suas colunas é o ID de uma profissão
+ * encontrada na tabela profissão, então temos que passar o ID de uma profissão que exista no banco como parâmetro aqui no create
+ * 
+ * Fazemos isso criando uma profissão e depois pegando o ID dela para usar aqui
  */
 $create = $conn->prepare("INSERT INTO usuario(nome, dataNascimento, idProfissao) VALUES (?,?,?)");
 $create->bind_param("ssi", $dadosUsuario['nome'], $dadosUsuario['dataNascimento'], $dadosUsuario['profissao']);
-$create->execute();
+// $create->execute();
 
 echo "<h1>Novo usuário</h1>";
-var_dump($create);
+// var_dump($create);
+
+
+/**
+ * Passando por todas as linhas e mostrando todos os resultados gerados
+ */
+if ($profssaoConsulta->num_rows > 0) {
+    while($row = $profssaoConsulta->fetch_assoc()) {
+        echo "ID: " . $row["idProfissao"] . " - Nome: " . $row["nome"] . "<br>";
+    }
+} else {
+    echo "Nenhum resultado encontrado.";
+}
